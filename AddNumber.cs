03492fba237b6b522265b33aa19e1aa34bd9b841
tejaswishah1 <tejaswishah1998@gmail.com>
2020-10-03 11:41:45 +0530

    [tejas] added Nlog Demo files

---
+++ AddNumber.cs
@@ -0,0 +1,26 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLogDemo
+{
+    public class AddNumber
+    {
+        NLogService NLog = new NLogService();
+        public int Sum(int a, int b)
+        {
+            if (a == 0 || b == 0)
+            {
+                NLog.LogDebug("Debug Successful : Sum()");
+                NLog.LogError("Expecting null values");
+                NLog.LogWarn("a & b shouldnot be equal to 0");
+            }
+                int c = a + b;
+                NLog.LogDebug("Debug Successful : Sum()");
+                NLog.LogInfo("Sum method passed, Result" + c);
+                return c;
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NLogDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition problem \n Enter a, b value");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            AddNumber add = new AddNumber();
+            Console.WriteLine("Result is " + add.Sum(a, b));
+        }
+      
+    }
+}
